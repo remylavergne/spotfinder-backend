b00fd993776d6ba38e4d25d4e63fb18da7d24449
Rémy Lavergne <lavergne.remy@gmail.com>
2020-09-17 20:23:26 +0200

    init project 🚀

---
+++ src/Application.kt
@@ -0,0 +1,58 @@
+package dev.remylavergne.spotfinder
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.features.*
+import org.slf4j.event.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.auth.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(Compression) {
+        gzip {
+            priority = 1.0
+        }
+        deflate {
+            priority = 10.0
+            minimumSize(1024) // condition
+        }
+    }
+
+    install(AutoHeadResponse)
+
+    install(CallLogging) {
+        level = Level.INFO
+        filter { call -> call.request.path().startsWith("/") }
+    }
+
+    install(DefaultHeaders) {
+        header("X-Engine", "Ktor") // will send this header with each response
+    }
+
+    install(Authentication) {
+        basic("myBasicAuth") {
+            realm = "Ktor Server"
+            validate { if (it.name == "test" && it.password == "password") UserIdPrincipal(it.name) else null }
+        }
+    }
+
+    routing {
+        get("/") {
+            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
+
+        authenticate("myBasicAuth") {
+            get("/protected/route/basic") {
+                val principal = call.principal<UserIdPrincipal>()!!
+                call.respondText("Hello ${principal.name}")
+            }
+        }
+    }
+}
+
