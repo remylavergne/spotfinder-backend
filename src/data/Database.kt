1e299877329dfc1c97d422f23e0c199797435680
Rémy Lavergne <lavergne.remy@gmail.com>
2020-09-17 20:43:51 +0200

    mongodb implementation
    mongodb implementation

---
+++ src/data/Database.kt
@@ -0,0 +1,38 @@
+package dev.remylavergne.spotfinder.data
+
+import com.mongodb.client.MongoClient
+import com.mongodb.client.MongoDatabase
+import io.ktor.application.*
+import io.ktor.util.*
+import org.litote.kmongo.KMongo
+
+@KtorExperimentalAPI
+object Database {
+
+    private lateinit var client: MongoClient
+    private lateinit var database: MongoDatabase
+
+    @Throws(Exception::class)
+    fun initialize(application: Application): Database {
+        val username = application.environment.config.property("mongodb.username")
+            .getString()
+        val password = application.environment.config.property(
+            "mongodb.password"
+        ).getString()
+        val hostname = application.environment.config.property("mongodb.hostname")
+            .getString()
+        val port = application.environment.config.property(
+            "mongodb.port"
+        ).getString()
+        val databaseName = application.environment.config.property("mongodb.name").getString()
+
+        try {
+            client = KMongo.createClient("mongodb://$username:$password@$hostname:$port")
+            database = client.getDatabase(databaseName)
+        } catch (e: Exception) {
+            throw e
+        }
+
+        return this
+    }
+}
