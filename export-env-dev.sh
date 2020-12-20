0346324de38e5818abfb8d23d084cdc5b0866c7f
Rémy LAVERGNE <lavergne.remy@gmail.com>
2020-12-20 14:47:20 +0100

    unused files

--- export-env-dev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export ADMIN_USERNAME=root
-export ADMIN_PASSWORD=root
-export MONGO_USER=remy
-export MONGO_PASS=remy
-export MONGO_HOST=localhost
-export MONGO_DATABASE_NAME=spotfinder
-export MONGO_PORT=27017
