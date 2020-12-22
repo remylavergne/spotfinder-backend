73b51d04a2c8e1a65ed013a1ac0538eba3885b0d
Rémy LAVERGNE <lavergne.remy@gmail.com>
2020-12-22 20:38:18 +0100

    update bash script for linux env

--- docker-prod-env/deployement.sh
+++ docker-prod-env/deployement.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 dockerfile="templates/Dockerfile"
 composeFile="templates/docker-compose.yml"
@@ -7,6 +7,19 @@ grafanaUserIdPattern="%grafana-user-id%"
 currentUserId=$(id -u)
 
 read -p 'Version to deploy (e.g. 0.0.3): ' version
+
+deleteFolder() {
+    read -p "$1" choice
+    if [ "$choice" = "y" ] || [ "$choice" = "Y" ]; then
+        sudo rm -rf $2
+        echo "Folder $2 deleted"
+    elif [ "$choice" = "n" ] || [ "$choice" = "N" ]; then
+        echo "No delete"
+    else
+        echo "Unknow choice..."
+        exit 0
+    fi
+}
 
 createFolder() {
     if [ -d "$1" ]; then
@@ -19,36 +32,10 @@ createFolder() {
 
 # Delete existing folders (TODO => Extract in a function)
 echo "--> Delete existing directories"
-read -p 'Delete pictures directory? [yY/nN]: ' deletePictures
-if [ "$deletePictures" = "y" ] || [ "$deletePictures" = "Y" ]; then
-    sudo rm -rf pictures
-elif [ "$deletePictures" = "n" ] || [ "$deletePictures" = "N" ]; then
-    echo "No delete"
-else
-    echo "Unknow choice..."
-    exit 0
-fi
-
-read -p 'Delete mongo directory? [yY/nN]: ' deleteMongo
-if [ $deleteMongo = "y" ] || [ $deleteMongo = "Y" ]; then
-    sudo rm -rf mongo
-elif [ $deleteMongo = "n" ] || [ $deleteMongo = "N" ]; then
-    echo "No delete"
-else
-    echo "Unknow choice..."
-    exit 0
-fi
-
-read -p 'Delete monitoring directory? [yY/nN]: ' deleteMonitoring
-if [ $deleteMonitoring = "y" ] || [ $deleteMonitoring = "Y" ]; then
-    sudo rm -rf monitoring
-elif [ $deleteMonitoring = "n" ] || [ $deleteMonitoring = "N" ]; then
-    echo "No delete"
-else
-    echo "Unknow choice..."
-    exit 0
-fi
-
+# read -p 'Delete pictures directory? [yY/nN]: ' deletePictures
+deleteFolder 'Delete pictures directory? [yY/nN]: ' pictures
+deleteFolder 'Delete mongo directory? [yY/nN]: ' mongo
+deleteFolder 'Delete monitoring directory? [yY/nN]: ' monitoring
 # Create folders
 echo "--> Create directories"
 createFolder pictures
@@ -57,8 +44,8 @@ createFolder monitoring/grafana
 # Update docker-compose.yml template
 echo "--> Generate Docker Compose via template"
 cp templates/docker-compose-template.yml templates/docker-compose_generate.yml
-sed -i '' "s/$imageVersionPattern/$version/gi" "templates/docker-compose_generate.yml"
-sed -i '' "s/$grafanaUserIdPattern/$currentUserId/gi" "templates/docker-compose_generate.yml"
+sed -i "s/$imageVersionPattern/$version/gi" "templates/docker-compose_generate.yml"
+sed -i "s/$grafanaUserIdPattern/$currentUserId/gi" "templates/docker-compose_generate.yml"
 mv templates/docker-compose_generate.yml ./docker-compose.yml
 # Move Prometheus configuration
 echo "--> Move Prometheus configuration"
