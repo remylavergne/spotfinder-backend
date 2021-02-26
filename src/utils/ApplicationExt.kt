d695262f456fab3cb2de2dea89b17d0250f3833b
Rémy LAVERGNE <lavergne.remy@gmail.com>
2021-02-26 20:06:42 +0100

    catch serialization error

--- src/utils/ApplicationExt.kt
+++ src/utils/ApplicationExt.kt
@@ -4,5 +4,10 @@ import io.ktor.application.*
 import io.ktor.request.*
 
 suspend inline fun <reified T> ApplicationCall.getResponseObject(): T? {
-    return MoshiHelper.fromJson<T>(this.receiveText())
+    return try {
+        MoshiHelper.fromJson<T>(this.receiveText())
+    } catch (e: Exception) {
+        println(e.localizedMessage)
+        null
+    }
 }
